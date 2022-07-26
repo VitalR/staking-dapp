b02f50a37a35170ed7496b1672c6aa7dfe52aa98
VitalR <vitalii.rodikov@gmail.com>
2022-07-26 14:03:16 +0300

    Getting Started Project

---
+++ hardhat.config.js
@@ -0,0 +1,40 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+const { PRIVATE_KEY_TESTNET, ALCHEMY_KEY, ETHERSCAN_API_KEY, PRIVATE_KEY_MAINNET } = process.env;
+
+module.exports = {
+  networks: {
+    rinkeby: { 
+      url: `https://eth-rinkeby.alchemyapi.io/v2/${ALCHEMY_KEY}`,
+      accounts: [PRIVATE_KEY_TESTNET]
+    }, 
+    mainnet: {
+      url: `https://eth-mainnet.alchemyapi.io/v2/${ALCHEMY_KEY}`,
+      accounts: [PRIVATE_KEY_MAINNET]
+    },
+  },
+  solidity: {
+    version: "0.8.10",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 500
+      }
+    }
+  },
+  paths: {
+    artifacts: ".client/src/artifacts"
+  },
+  gasPrice: "10000000000",
+  gas: "auto",
+  gasReporter: {
+    gasPrice: 1,
+    enabled: true,
+    showTimeSpent: true,
+    currency: "USD",
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY
+  }
+};
