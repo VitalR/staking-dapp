0154270d7bd397e1fa1cc434e45500cc2dcb85d4
VitalR <vitalii.rodikov@gmail.com>
2022-07-27 15:51:48 +0300

    Update deployment script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,28 +1,44 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
+const { waffle } = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  [owner, signer1, signer2, _] = await ethers.getSigners()
 
-  const lockedAmount = hre.ethers.utils.parseEther("1");
+  Staking = await ethers.getContractFactory('Staking')
+  staking = await Staking.deploy({ value: ethers.utils.parseEther('10') })
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  console.log("Staking contract deployed to: ", staking.address, "by ", owner.address);
 
-  await lock.deployed();
+  const provider = waffle.provider
+  let data
+  let transaction
+  let receipt
+  let block
+  let newUnlockDate
 
-  console.log("Lock with 1 ETH deployed to:", lock.address);
+  data = { value: ethers.utils.parseEther('0.5') }
+  transaction = await staking.connect(signer1).stakeEther(30, data)
+
+  data = { value: ethers.utils.parseEther('1') }
+  transaction = await staking.connect(signer1).stakeEther(180, data)
+
+  data = { value: ethers.utils.parseEther('1.75') }
+  transaction = await staking.connect(signer1).stakeEther(180, data)
+
+  data = { value: ethers.utils.parseEther('5') }
+  transaction = await staking.connect(signer1).stakeEther(90, data)
+  receipt = await transaction.wait()
+  block = await provider.getBlock(receipt.blockNumber)
+  newUnlockDate = block.timestamp - (60 * 60 * 24 * 100)
+  await staking.connect(owner).changeUnlockDate(3, newUnlockDate)
+
+  data = { value: ethers.utils.parseEther('1.75') }
+  transaction = await staking.connect(signer1).stakeEther(180, data)
+  receipt = await transaction.wait()
+  block = await provider.getBlock(receipt.blockNumber)
+  newUnlockDate = block.timestamp - (60 * 60 * 24 * 100)
+  await staking.connect(owner).changeUnlockDate(4, newUnlockDate)
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
